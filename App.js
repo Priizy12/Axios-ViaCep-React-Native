a82d889018749c18d3abfd55324d881cc89aa6a4
Priizy12 <ivailsonqueiroz9@gmail.com>
2024-01-22 21:00:10 -0300

    Praticando

--- App.js
+++ App.js
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState,  useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, SafeAreaView, Keyboard } from 'react-native';
+import api from './src/axios/api'
 
 export default function App() {
+
+  const [cep, setCep] = useState('');
+  const inputRef = useRef(null);
+  const [cepUser, setCepUser] = useState(null)
+
+  async function Buscar() {
+    if (cep == '') {
+      alert('Informe um cep Valido');
+      setCep('');
+      return;
+    }
+    try {
+      const response = await api.get(`/${cep}/json`);
+      setCepUser(response.data)
+      Keyboard.dismiss()
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  function Limpar() {
+    setCep('')
+    inputRef.current.focus()
+    setCepUser(null)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.text}>Digite o cep desejado</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: 62785000"
+          value={cep}
+          onChangeText={(text) => setCep(text)}
+          keyboardType="numeric"
+          ref={inputRef}
+        />
+      </View>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={[styles.btn, { backgroundColor: '#1d75cd' }]} onPress={Buscar} >
+          <Text style={styles.btnText}>Buscar</Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity style={[styles.btn, { backgroundColor: '#cd3e1d' }]} onPress={Limpar}>
+          <Text style={styles.btnText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+
+      {cepUser &&
+        <View style={styles.result}>
+          <Text style={styles.itemText}>CEP: {cepUser.cep} </Text>
+          <Text style={styles.itemText}>Logradouro: {cepUser.logradouro}</Text>
+          <Text style={styles.itemText}>Bairro:  {cepUser.bairro}</Text>
+          <Text style={styles.itemText}>Cidade: {cepUser.localidade} </Text>
+          <Text style={styles.itemText}>Estado:  {cepUser.uf} </Text>
+        </View>
+      }
+
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  text: {
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: 'white',
+    borderWidth: 0.8,
+    borderColor: 'gray',
+    borderRadius: 5,
+    width: '90%',
+    padding: 10,
+    fontSize: 17
+  },
+  btnArea: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    justifyContent: 'space-around'
+  },
+  btn: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 5
+  },
+  btnText: {
+    fontSize: 15,
+    color: 'white'
+  },
+  result: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  itemText: {
+    fontSize: 20
+  }
 });
---
+++ README.md
@@ -0,0 +1,40 @@
+# 🚀 Sistema de Consumo de API do VIACEP
+
+Este é um projeto simples que consome a API do VIACEP. O usuário pode digitar o CEP no campo exigido e, ao clicar em Buscar, todas as informações daquele CEP serão exibidas na tela.
+
+## 📚 Biblioteca Utilizada
+
+Este projeto foi construído utilizando a seguinte biblioteca:
+
+- `axios`: Esta biblioteca foi usada para consumir a API do VIACEP. Além disso, foram utilizados hooks para seguir as boas práticas.
+
+## 🚀 Como Iniciar a Aplicação
+
+Siga os passos abaixo para executar a aplicação:
+
+1. **Instale as dependências do projeto**: Se você não tiver a pasta `node_modules`, pode instalar as dependências com o seguinte comando:
+
+```bash
+npm install
+# ou
+yarn install
+```
+
+2. **Inicie a aplicação**: Após a instalação das dependências, você pode iniciar a aplicação com o seguinte comando:
+
+```bash
+ yarn dev
+```
+
+## Contribuição 🤝
+
+Sinta-se à vontade para contribuir para o desenvolvimento deste projeto. Qualquer contribuição é bem-vinda!
+
+Fork o projeto
+- Crie uma nova branch **(git checkout -b feature/sua-feature)**
+- Faça commit das suas mudanças **(git commit -m 'Adicione uma nova feature')**
+- Envie as mudanças para o repositório remoto **(git push origin feature/sua-feature)**
+- Abra um pull request!
+
+
+
